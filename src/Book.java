961b8e7a502d5a49fb8e47e830d5de7c2f40f6f7
Westender <javaxwestender@gmail.com>
2023-08-15 17:19:46 +0300

    Fix : author get/setters

--- src/Book.java
+++ src/Book.java
@@ -1,9 +1,9 @@
 public class Book {
     private String title;
-    private String author;
+    private Author author;
     private int publicationYear;
 
-    public Book(String title, String author, int publicationYear) {
+    public Book(String title, Author author, int publicationYear) {
         this.title = title;
         this.author = author;
         this.publicationYear = publicationYear;
@@ -17,11 +17,11 @@ public class Book {
         this.title = title;
     }
 
-    public String getAuthor() {
+    public Author getAuthor() {
         return author;
     }
 
-    public void setAuthor(String author) {
+    public void setAuthor(Author author) {
         this.author = author;
     }
 
